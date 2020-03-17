7d3eb9bb5ffebd6b8df6989386d2707718d188e1
Santiago3 <3055242489@qq.com>
2020-03-17 12:21:42 +0800

    neo4j-database established

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # GraduationProject
 Project for my Graduation paper
 
-csv_process下文件用于处理下载的ethereum数据文件(.csv)
+本project中使用的图数据库为neo4j图数据库(社区版本)
+下载地址：https://neo4j.com/download-center/#community
 
-ldb_process下文件用于处理geth同步ethereum数据文件(.ldb)——放弃这种做法了
+csv_process下文件用于处理下载的ethereum数据文件(.csv)
+本project中使用的ethereum数据文件为Block文件和NormalTransaction文件
+下载地址：http://xblock.pro/ethereum-cn/
+
+ldb_process下文件用于处理geth同步ethereum数据文件(.ldb)——后放弃这种做法
+本project中使用的本地同步ethereum的工具为geth
+下载地址：https://geth.ethereum.org/downloads/
--- csv_process/client_to_transaction.py
+++ csv_process/client_to_transaction.py
@@ -15,11 +15,11 @@ newheader_in = ['clientid', 'transactionid', 'relationship']
 data_in.to_csv(dest_filename_in, index = False, header = newheader_in)
 print("relationship_from generated!")
 
-#transaction_to_client存在client为空的情况(例如第55行数据)，这也导致出现有节点为client的情况——尚未解决
-source_out = pd.read_csv(source_filename, usecols = ['id:ID(transaction)', 'to'], low_memory=False)
+#transaction_to_client存在client为空的情况(例如第55行数据)，这也导致出现有节点为client的情况
+source_out = pd.read_csv(source_filename, usecols = ['id', 'to'], low_memory=False)
 source_out['relationship'] = 'out'
 source_out['to'] = source_out['to'].astype(str)
-source_out['to'] = source_out['to'].apply(lambda x :((x == '')?'client':('client' + x)))
+source_out['to'] = source_out['to'].apply(lambda x :'client' if x == 'nan' else('client' + x))
 #data_out = pd.DataFrame()
 #data_out = (source_out.loc[:,['id', 'to', 'relationship']])
 newheader_out = ['transactionid', 'clientid', 'relationship']
