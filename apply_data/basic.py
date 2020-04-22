1995ca29044577461598d70e8e1473668be92741
Santiago3 <468102981qq@gmail.com>
2020-04-22 21:12:48 +0800

    test connection

--- apply_data/basic.py
+++ apply_data/basic.py
@@ -1,7 +1,12 @@
-# -*- coding:utf-8 -*-
-import os
-from py2neo import Graph, Node, Relationship, NodeSelector 
-graph = Graph("http://localhost:7474", username="neo4j", password=',5d0_74f')
+# encoding='utf8'
+import py2neo
+from py2neo import Graph, Node, Relationship, PropertyDict
+
+print(py2neo.__version__)
+
+graph = Graph("http://127.0.0.1:7474", username="neo4j", password=",5d0_74f")
+#graph = Graph("http://neo4j:,5d0_74f@localhost:7474/db/data")
+#graph = Graph("http:127.0.0.1:7474")
 
 data1 = graph.data('MATCH (transaction {timestamp: "0x57582e1c", label: "transaction"})-[belong]->(block {label: "block"}) return block')
 print("data1 = ", data1, type(data1))
