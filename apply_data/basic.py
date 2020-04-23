4337af535c7b3f4b68a8ee3de96b4511257531ab
Santiago233 <468102981qq@gmail.com>
2020-04-23 16:30:48 +0800

    basic.

--- apply_data/basic.py
+++ apply_data/basic.py
@@ -7,8 +7,8 @@ def Client_find_degree():
 	print("请输入节点的address")
 	address = input()
 	in_count = out_count = 0
-	transaction_in = graph.data('match(client {label:'+label+',id:'+id_+',address:'+address+'})-[in]->(transaction) return transaction')
-	transaction_out = graph.data('match(transaction)-[out]->(client {label:'+label+',id:'+id_+',address:'+address+'}) return transaction')
+	transaction_in = graph.data('match(client {label:"'+ label +'",id:"'+ id_ +'",address:"'+ address +'"})-[in]->(transaction) return transaction')
+	transaction_out = graph.data('match(transaction)-[out]->(client {label:"'+ label +'",id:"'+ id_ +'",address:"'+ address +'"}) return transaction')
 	for transaction in transaction_in:
 		in_count += 1
 	for transaction in transaction_out:
