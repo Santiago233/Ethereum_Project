c7dc1f541ee2fb6d78ff9497174c088176a9519f
Santiago233 <468102981qq@gmail.com>
2020-04-27 15:37:35 +0800

    transfer_money

--- apply_data/higher.py
+++ apply_data/higher.py
@@ -1,5 +1,6 @@
 from graph import graph
 import basic
+import pandas as pd
 
 def Client_whether_launder():
 	print("TODO")
@@ -12,13 +13,13 @@ def Client_whether_theft():
 	for transaction in transaction_in:
 		concrete_transaction = {}
 		concrete_transaction["time"] = transaction["transaction"]["timestamp"]
-		concrete_transaction["value"] = transaction["transaction"]["value"]
+		concrete_transaction["value"] = len(transaction["transaction"]["value"])
 		concrete_transaction["way"] = "from"
 		transactions.append(concrete_transaction)
 	for transaction in transaction_out:
 		concrete_transaction = {}
 		concrete_transaction["time"] = transaction["transaction"]["timestamp"]
-		concrete_transaction["value"] = transaction["transaction"]["value"]
+		concrete_transaction["value"] = len(transaction["transaction"]["value"])
 		concrete_transaction["way"] = "to"
 		transactions.append(concrete_transaction)
 	new_transactions = sorted(transactions, key = lambda e:e["time"])
@@ -33,6 +34,27 @@ def Client_whether_tax_evasion():
 		print("该节点可能作为避税港/资产转移地")
 	else:
 		print("该节点应该不是避税港/资产转移地")
+
+def Client_whether_transfer_money():
+	print("请输入节点的address")
+	address = input()
+	transaction_in, transaction_out = basic.Client_get_transaction_by_property(address)
+	values = []
+	for transaction in transaction_in:
+		#value数据普遍位数较多，故使用长度作为判断
+		value = len(transaction["transaction"]["value"])
+		print(value)
+		values.append(value)
+	values_p = pd.Series(values).describe()
+	Q1 = values_p['25%']
+	Q3 = values_p['75%']
+	IQR = Q3 - Q1
+	values_upper = Q3 + 1.5 * IQR
+	for value in values:
+		if(value - values_upper >= 2.5):
+			print("该节点可能发生资产转移行为")
+			return
+	print("该节点应该尚未发生资产转移行为")
 
 def Client_whether_illegal_transaction():
 	transaction_in, transaction_out = basic.Client_get_transaction()
@@ -64,8 +86,9 @@ def higher_api():
 	print("1.查询图中某个节点是否可能发生洗钱交易")
 	print("2.查询图中某个节点是否可能被盗用账号")
 	print("3.查询图中某个节点是否可能作为避税港/资产转移地")
-	print("4.查询图中某个节点是否可能作为非法交易组织者")
-	print("5.返回上一级")
+	print("4.查询图中某个节点是否可能发生资产转移行为")
+	print("5.查询图中某个节点是否可能作为非法交易组织者")
+	print("6.返回上一级")
 	select_two = int(input())
 	if(select_two == 1):
 		#print("TODO1")
@@ -78,8 +101,11 @@ def higher_api():
 		Client_whether_tax_evasion()
 	elif(select_two == 4):
 		#print("TODO4")
-		Client_whether_illegal_transaction()
+		Client_whether_transfer_money()
 	elif(select_two == 5):
+		#print("TODO4")
+		Client_whether_illegal_transaction()
+	elif(select_two == 6):
 		print("exit")
 	else:
 		print("Wrong!请重新选择具体查询方式(输入数字)")
